4dc879c7a4c28843ca33c275491872cd4ca155a4
Alapaap.os <julianmanuelle@gmail.com>
2025-06-11 20:13:50 +0800

    Add files via upload
    Day 5

---
+++ Day5-PasswordGenerator.py
@@ -0,0 +1,27 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+RawPassword = []
+Password = ""
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+for i in range(nr_letters):
+    RawPassword.append(letters[random.randint(0,len(letters)-1)])
+for i in range(nr_symbols):
+    RawPassword.append(symbols[random.randint(0,len(symbols)-1)])
+for i in range(nr_numbers):
+    RawPassword.append(numbers[random.randint(0,len(numbers)-1)])
+
+random.shuffle(RawPassword)
+
+for char in RawPassword:
+    Password += char
+
+print(Password)
